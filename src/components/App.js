e178cb1198e0845d20ff0c91d53c3be46d59d194
Monica <monica@DESKTOP-A3SK9UM.localdomain>
2024-01-31 01:48:36 -0800

    done with assignement

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
+import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
 console.log(user);
 
 function App() {
+  const { bio, github, linkedin } = user;
   return (
     <div>
+      <Links github={user.github} linkedin={user.linkedin} />
       <NavBar />
-      <Home />
-      <About />
+      <Home user={user} />
+      <About bio={bio} github={github} linkedin={linkedin} />
     </div>
   );
 }
